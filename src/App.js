5d9712d99127def8768aa64fed6f3ff0cc870506
Alex <Aleks031994@Mail.ru>
2021-06-24 16:40:03 +0300

    added Features  components with styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Header from "./Components/Header/Header";
 import Main from "./Components/Main/Main";
+import Features from "./Components/Features/Features";
 
 function App() {
-  return (
-    <div>
-      <Header/>
-        <Main/>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Main/>
+            <Features/>
+        </div>
+    );
 }
 
 export default App;
